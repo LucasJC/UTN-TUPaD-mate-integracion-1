baf70d60319f57e6a4bede811c260f6258fa0f73
LucasJC <lucas.couchot@gmail.com>
2025-04-16 20:54:19 -0500

    actualizar README

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # UTN-TUPaD-mate-integracion-1
-Trabajo práctico para Semana de integración 1 entre Matemáticas y Programación 1
+Trabajo práctico para Semana de integración 1 entre Matemáticas y Programación 1.
+
+## Problema
+El problema elegido es el de conversión entre bases numéricas.
+Este código consiste en un conversor de decimal a hexadecimal.
+
+## Ejecución
+Como prerequisito es necesario tener python instalado.
+Luego basta con ejecutar el siguiente comando en una consola:
+```bash
+python .\proyecto.py
+```
--- proyecto.py
+++ proyecto.py
@@ -1,4 +1,4 @@
-#Se define la funcion de conversion a hexadecimal
+# se define la función de conversión a hexadecimal
 def decimal_a_hexadecimal(numero_decimal):
     # definimos un string con todos los dígitos en su índice correspondiente
     caracteres_hexa = "0123456789ABCDEF"
@@ -10,7 +10,7 @@ def decimal_a_hexadecimal(numero_decimal):
         # calculamos residuo de dividir por la base (16)
         residuo = numero_decimal % 16
         # determinamos el caracter hexa correspondiente al residuo que está en decimal
-        caracter_hexa = caracteres_hexa[residuo] # el residuo es el índice del caracter hexa en el string 'hex_chars'
+        caracter_hexa = caracteres_hexa[residuo] # el residuo es el índice del caracter hexa en el string 'caracteres_hexa'
         # concatenamos el caracter al inicio del string de resultado
         numero_hexa = caracter_hexa + numero_hexa
         # reemplazamos el valor de decimal con el valor entero de la división por la base
@@ -18,9 +18,9 @@ def decimal_a_hexadecimal(numero_decimal):
     # retornamos el string resultante, ya en hexadecimal
     return numero_hexa
 
-#Se le pide al usuario que ingrese el numero que desea convertir
-numero = int(input("Ingrese un numero para convertir: "))
-#Se ejecuta la funcion de conversion, utilizando el input del usuario
+# se le pide al usuario que ingrese el número que desea convertir
+numero = int(input("Ingrese un numero decimal para convertir: "))
+# se ejecuta la función de conversión, utilizando el input del usuario
 resultado = decimal_a_hexadecimal(numero)
-#Se muestra en pantalla el resultado de la conversion. 
-print(f"El resultado en Hexadecimal es: {resultado}")
+# se muestra en pantalla el resultado de la conversión. 
+print(f"El resultado en Hexadecimal es: {resultado}\u2081\u2086")
